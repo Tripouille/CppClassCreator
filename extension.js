4b394c35809ce98e56c18fc0e6cdaf63c9242148
jgambard <jgambard@student.42lyon.fr>
2020-07-14 13:55:44 +0200

    v0.1

--- extension.js
+++ extension.js
@@ -21,22 +21,23 @@ function activate(context) {
 		const folderPath = vscode.workspace.workspaceFolders[0].uri
 			.toString()
 			.split(":")[1];
-		let hppContent = '#ifndef CLASS_HPP \n\
+		let hppGuard = '#ifndef CLASS_HPP \n\
 # define CLASS_HPP\n\
-\n\
-class CLASS \n\
+\n';
+		let hppContent = 'class CLASS \n\
 {\n\
 	public:\n\
 		CLASS(void);\n\
 		~CLASS(void);\n\
-		CLASS(const CLASS &other);\n\
-		CLASS               &operator=(const CLASS &other);\n\
+		CLASS(CLASS const &other);\n\
+\n\
+		CLASS		&operator=(CLASS const &other);\n\
 \n\
 	private:\n\
 };\n\
 \n\
 #endif';
-		let cppContent = "#include \"CLASS.hpp\" \n\
+		let cppContent = '#include "CLASS.hpp" \n\
 \n\
 CLASS::CLASS(void)\n\
 {\n\
@@ -46,30 +47,31 @@ CLASS::~CLASS(void)\n\
 {\n\
 }\n\
 \n\
-CLASS::CLASS(const CLASS &other)\n\
+CLASS::CLASS(CLASS const &other)\n\
 {\n\
 }\n\
 \n\
-CLASS		&CLASS::operator=(const CLASS &other)\n\
+CLASS		&CLASS::operator=(CLASS const &other)\n\
 {\n\
-}";
+}';
 		vscode.window.showInputBox(options).then((value) =>
 		{
-			fs.writeFile(path.join(folderPath, value + ".hpp"), hppContent.replace(/CLASS/g, value), err => {
+			let capitalized = value[0].toUpperCase() + value.slice(1).toLowerCase();
+			fs.writeFile(path.join(folderPath, capitalized + ".hpp"), hppGuard.replace(/CLASS/g, value.toUpperCase()) + hppContent.replace(/CLASS/g, capitalized), err => {
 				if (err)
 				{
 					console.error(err);
 					return ;
 				}
-				vscode.window.showInformationMessage(value + ".hpp created.");
+				vscode.window.showInformationMessage(capitalized + ".hpp created.");
 			})
-			fs.writeFile(path.join(folderPath, value + ".cpp"), cppContent.replace(/CLASS/g, value), err => {
+			fs.writeFile(path.join(folderPath, capitalized + ".cpp"), cppContent.replace(/CLASS/g, capitalized), err => {
 				if (err)
 				{
 					console.error(err);
 					return ;
 				}
-				vscode.window.showInformationMessage(value + ".cpp created.");
+				vscode.window.showInformationMessage(capitalized + ".cpp created.");
 			})
 		});
 	});
